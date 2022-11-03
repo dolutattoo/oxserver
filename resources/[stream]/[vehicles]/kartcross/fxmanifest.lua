3f11b449d4eb3ab405670869d04146284fdbfc0a
Dolu <alfrider02@gmail.com>
2022-11-04 00:29:13 +0100

    feat(stream): not ignore kartcross

---
+++ resources/[stream]/[vehicles]/kartcross/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description "Kart cross from GTA5-mods"
+author "louping0"
+version '1.1'
+url "https://www.gta5-mods.com/vehicles/kart-cross-beta"
+
+files {
+    'handling.meta',
+    'vehicles.meta',
+    'carcols.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
