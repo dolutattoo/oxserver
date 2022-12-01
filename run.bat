3d547d5c8c3fecaf87925f835615dbeb20471a8d
Dolu <alfrider02@gmail.com>
2022-12-02 00:55:26 +0100

    update

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 
 tasklist | find /i "FXServer.exe" && taskkill /im FXServer.exe /F
 @echo on
-D:\FiveM\fivem-servers\artifacts\5904\FXServer.exe +exec server.cfg
+D:\FiveM\fivem-servers\artifacts\6103\FXServer.exe +exec server.cfg
